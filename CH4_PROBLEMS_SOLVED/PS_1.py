67feb7676c0fa1a54da34b15da3f0bcc812ea952
Aayush Garg <aayushgarg635@gmail.com>
2026-09-05 14:40:59 +0530

    CH 4 PRACTICE SET

---
+++ CH4_PROBLEMS_SOLVED/PS_1.py
@@ -0,0 +1,37 @@
+fruits=[]
+
+f1=input("Enter Fruit Name:  ")
+fruits.append(f1)
+f2=input("Enter Fruit Name:  ")
+fruits.append(f2)
+f3=input("Enter Fruit Name:  ")
+fruits.append(f3)
+f4=input("Enter Fruit Name:  ")
+fruits.append(f4)
+f5=input("Enter Fruit Name:  ")
+fruits.append(f5)
+f6=input("Enter Fruit Name:  ")
+fruits.append(f6)
+f7=input("Enter Fruit Name:  ")
+fruits.append(f7)
+
+print(fruits)
+vegetables = []
+
+v1=input("ENTER VEGETABLE NAME: ")
+vegetables.append(v1)
+v2=input("ENTER VEGETABLE NAME: ")
+vegetables.append(v2)
+print(vegetables)
+
+# marks = {}
+# m1=input("Enter first student marks: ")
+# marks.appened(m1)
+# m2=input("Enter first student marks: ")
+# marks.appened(m2)
+# m3=input("Enter first student marks: ")
+# marks.appened(m3)
+# m4=input("Enter first student marks: ")
+# marks.appened(m4)
+# print(marks)
+# COMMENTED CODES ARE GIVING ERROR
---
+++ CH4_PROBLEMS_SOLVED/PS_3.py
@@ -0,0 +1,3 @@
+a = (23, 34 , True , "gunnu")
+print(a)
+a[2]=21
