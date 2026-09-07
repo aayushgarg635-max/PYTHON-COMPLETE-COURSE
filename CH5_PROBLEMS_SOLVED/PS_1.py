a9f14452b5363de1c5b213e030222dcd249513e5
Aayush Garg <aayushgarg635@gmail.com>
2026-09-07 19:53:29 +0530

    CH 5 PRACTICE SET

---
+++ CH5_PROBLEMS_SOLVED/PS_1.py
@@ -0,0 +1,18 @@
+words={
+    "madad":"help",
+    "kursi":"chair",
+    "kela":"banana",
+    "seb":"apple",
+    "duniya":"world",
+    "takat":"power"
+}
+word = input("Enter the word you want meaning of: ")
+print (words[word])
+
+words={
+    "aadmi":"men",
+    "zimmedari":"responsilbility",
+    "takat":"power"
+}
+word = input("enter the word you want meaning of:  ")
+print(words[word])
