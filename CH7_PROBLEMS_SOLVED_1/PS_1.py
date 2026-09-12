fe872b7b5bd8b67f92cc87de8ea7e83a81bfab56
Aayush Garg <aayushgarg635@gmail.com>
2026-09-12 20:58:52 +0530

    CH 7 PRACTICE SET PART 1

---
+++ CH7_PROBLEMS_SOLVED_1/PS_1.py
@@ -0,0 +1,15 @@
+n = int( input("Enter the number : "))
+
+for i in range(1,11):
+    print(f"{i} x {n} = {n*i} ")
+
+n = int(input("Enter your number: "))
+
+for i in range(1,11):
+    print(f"{n} x {i} = {n*i}")
+
+n=int(input("Enter your number: "))
+
+for i in range(1,11):
+    print(f"{n} x {i} = {n*i} ")
+
---
+++ CH7_PROBLEMS_SOLVED_1/PS_3.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the number : "))
+
+i = 1
+while(i<11):
+    print(f"{i} x {n} = {n*i}")
+    i += 1
