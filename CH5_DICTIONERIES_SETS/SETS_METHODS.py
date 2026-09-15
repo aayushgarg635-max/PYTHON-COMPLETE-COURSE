389cdbc2d133cb23d08e9f6a8921a94260686457
Aayush Garg <aayushgarg635@gmail.com>
2026-09-15 11:55:52 +0530

    Update SETS_METHODS.py

--- CH5_DICTIONERIES_SETS/SETS_METHODS.py
+++ CH5_DICTIONERIES_SETS/SETS_METHODS.py
@@ -1,11 +1,21 @@
+#1 Creating a set - duplicates are automatically removed
 s = {1,2,4,5,54,5,6,2,2,2}
+print(s) # output:{1,2,4,5,6,54}
+#2 Adding elements
 s.add(24)
 s.add(12)
-print(s)
+print(f"after adding 24 and 12 :{s}")
+#3 Removing - remove() and discard()
 s.remove(4)
-print(s)
-print(type(s))
-print(len(s))
-s.clear()    
+print(f"after removing 4:{s}")
+#dicard():it will not give error if value not found
+s.discard(10)
+print(f"after discard:{s}")
+# Len and type.
+print(f"length of set is:{len(s)}")
+print(f" type of s:{type(s)}")
+# clear:empties the set
+s.clear()
+print(f" after clear:{s}")
 
 
