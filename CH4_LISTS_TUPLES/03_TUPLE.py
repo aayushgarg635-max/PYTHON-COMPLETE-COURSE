f212abe18307d8db31260ca2aa6df11f27e99724
Aayush Garg <aayushgarg635@gmail.com>
2026-09-15 12:09:27 +0530

    Update 03_TUPLE.py

--- CH4_LISTS_TUPLES/03_TUPLE.py
+++ CH4_LISTS_TUPLES/03_TUPLE.py
@@ -1,7 +1,7 @@
 a = (1,2,4,6,8,9)
 print(a)
 print(type(a))
-a=(1)
+a=(1) # this is not tuple , this is int
 print(type(a))
-a = (1,)
+a = (1,) # this is tuple with one element and needs a comma
 print(type(a))
