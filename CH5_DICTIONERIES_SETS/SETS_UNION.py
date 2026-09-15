619f11bd7c4fee926bb86453ad5162363106fd9c
Aayush Garg <aayushgarg635@gmail.com>
2026-09-15 12:01:53 +0530

    Update SETS_UNION.py

--- CH5_DICTIONERIES_SETS/SETS_UNION.py
+++ CH5_DICTIONERIES_SETS/SETS_UNION.py
@@ -1,4 +1,6 @@
+# Union and Intersections in sets
 s1= {2,4,8,12}
 s2 ={4,10,16}
-print(s1.union(s2))
-print(s1.intersection(s2))
+print(s1.union(s2)) # {2,4,8,10,12,16}
+print(s1.intersection(s2)) #{4}
+
