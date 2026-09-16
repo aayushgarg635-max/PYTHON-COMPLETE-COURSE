4c928bdc9084fdd07d83cd0a9d07a61d535a7bf5
Aayush Garg <aayushgarg635@gmail.com>
2026-09-16 18:58:57 +0530

    Update Default_Argument.py

--- CH8_FUNCTIONS_RESCURSSIONS/Default_Argument.py
+++ CH8_FUNCTIONS_RESCURSSIONS/Default_Argument.py
@@ -1,5 +1,8 @@
+# ending = "thank you" is the default argument
 def goodday(name,ending="thank you"):
     print(f"good day",(name))
-
+# default values
 goodday("harry")
 goodday("aayush")
+#over riding the default value
+goodday("aayush","have a nice day")
