aa1b7d27b27b71c65565d7fe4e7ce4c97a0da778
Aayush Garg <aayushgarg635@gmail.com>
2026-09-16 18:54:57 +0530

    Update 04_TUPLES_METHODS.py

--- CH4_LISTS_TUPLES/04_TUPLES_METHODS.py
+++ CH4_LISTS_TUPLES/04_TUPLES_METHODS.py
@@ -1,18 +1,22 @@
+# tuples are immutables,uses()
 a = (1,2,4,6,8,9)
 print(a)
-print(type(a))
-
+print(type(a)) #class'tuple'
+# count(x)-how many times x appeared
 no = a.count(45)
-print(no)
+print(no) # 0 as 45 is not in the tuple
+#index(x)- first index of x
 i=a.index(4)
 print(i)
+# len()- length of tuple
 print(len(a))
 b =(2,3,4,5,67,8,9)
 print(b)
 print(type(b))
-no=b.count(5)
-print(no)
+no=b.count(5) #1
+print(no) #7
 print(len(b))
+# index with duplicates
 t = ("a", "b", "c", "b")
 t.index("b")      # 1
 t.index("b", 2)   # 3
