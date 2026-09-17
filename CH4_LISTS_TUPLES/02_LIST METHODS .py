7859a50be971de2b8a76419ff81a4d7221bba0b9
Aayush Garg <aayushgarg635@gmail.com>
2026-09-17 19:39:04 +0530

    Update 02_LIST METHODS .py

--- CH4_LISTS_TUPLES/02_LIST METHODS .py
+++ CH4_LISTS_TUPLES/02_LIST METHODS .py
@@ -1,9 +1,9 @@
 # lists can have mixed datatypes and indexing
-freinds = ["apple","orange",276,True,False , "variables"]
+friends = ["apple","orange",276,True,False , "variables"]
 print(freinds[0]) #apple
 #append()-add items at the end
-freinds.append("harry potter")
-print(freinds)
+friends.append("harry potter")
+print(friends)
 #sort()-sorts in ascending manner
 l1=[2,44,35,24,97]
 l1.sort()
@@ -16,9 +16,9 @@ print(l2)
 l3=[2,44,35,24,97]
 l3.insert(2,245) # INSERT 245 IN THE LIST AT INDEX 2
 print(l3)
-#remove(value) removes first occurence of value 
 l3.pop(2)
 print(l3)
+#remove(value) removes first occurence of value 
 l3.remove(44)
 print(l3)
 
