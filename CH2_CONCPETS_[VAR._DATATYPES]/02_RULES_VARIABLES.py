ea91b763f799ca31dc6755ccdcba4cc8b7deb8fd
Aayush Garg <aayushgarg635@gmail.com>
2026-09-18 22:16:43 +0530

    Update 02_RULES_VARIABLES.py

--- CH2_CONCPETS_[VAR._DATATYPES]/02_RULES_VARIABLES.py
+++ CH2_CONCPETS_[VAR._DATATYPES]/02_RULES_VARIABLES.py
@@ -3,6 +3,6 @@ _variables = 17
 keywords = 29
 _strings = 7
 
-# 6pooja=45  #   DUE TO STRTING WITH STARTING WITH  NUMBER
+# 6pooja=45  #   Due to string started with number
 # @sammer=56 # invalid due to @ symbol
 # s@meer=65 # invalid due to @ symbol
