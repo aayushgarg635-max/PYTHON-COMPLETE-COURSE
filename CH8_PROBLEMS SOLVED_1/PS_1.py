7abc2437282d1f868e187e01614e70db4fc9ef26
Aayush Garg <aayushgarg635@gmail.com>
2026-09-20 19:21:07 +0530

    CH 8 PRACTICE SET-1

---
+++ CH8_PROBLEMS SOLVED_1/PS_1.py
@@ -0,0 +1,19 @@
+
+def greatest(a,b,c):
+    if(a>b and a>c):
+        return a
+    elif(b>c and b>a):
+        return b
+    elif(c>a and c>b):
+        return c
+
+a = 2
+b = 4
+c = 7
+
+print(greatest(a,b,c))
+
+print("End of the programm")
+
+    
+
