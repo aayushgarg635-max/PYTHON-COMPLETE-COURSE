a47337a979ef833f43a54710b3d7d959ae8611dc
Aayush Garg <aayushgarg635@gmail.com>
2026-09-23 20:21:29 +0530

    Update 01_INTRO_TO_STRINGS.py

--- Ch3_STRINGS/01_INTRO_TO_STRINGS.py
+++ Ch3_STRINGS/01_INTRO_TO_STRINGS.py
@@ -1,9 +1,9 @@
-name = "harry"
+name_short = "harry"
 
-nameshot = name [0:3] # Start from index 0 to all till 3(excluding 3) 
-print (nameshot)
-character1 =name[1]
-print(character1)  
+nameshot = name_short [0:3] # Start from index 0 to all till 3(excluding 3) 
+print (nameshot) # har
+character1 =name_short[1]
+print(character1)  #a
 a = 'harry' # Single quoted string
 b = "harry" # Double quoted string
 c = '''harry''' # Triple quoted string
